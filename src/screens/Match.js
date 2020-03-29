56e1217b7bd9f3a40b0d2ec5dd8d8562900b3f21
GaryZLi <gary.zecun@gmail.com>
2020-03-29 06:36:41 -0700

    got entire app working

--- src/screens/Match.js
+++ src/screens/Match.js
@@ -38,7 +38,6 @@ export default class Match extends Component {
           </View>
           <Text style={styles.title}>It's a match!</Text>
           <View style={styles.imageContainer}>
-<<<<<<< HEAD
             <Image
               style={styles.image}
               source={{ uri: this.props.data.imageUrl }}
@@ -50,23 +49,6 @@ export default class Match extends Component {
           <TouchableOpacity style={styles.buttonContainer} onPress={() => this.getDirection()}>
             <Text style={styles.buttonText}>GET DIRECTIONS</Text>
           </TouchableOpacity>
-=======
-        <Image
-          style={styles.image}
-          source={require("../components/picSrc/restaurant.png")}
-        ></Image>
-      </View>
-      <Text style={styles.name}>Restaurant Name</Text>
-        <View style={styles.descriptionContainerOne}>
-          <Text style={styles.description}>$$$</Text>
-          <Text style={styles.description}>5 stars</Text>
-          <Text style={styles.description}>5,000 reviews</Text>
-        </View>
-        
-        <TouchableOpacity style={styles.buttonContainer}>
-                        <Text style={styles.buttonText}>GET DIRECTIONS</Text>
-                    </TouchableOpacity>
->>>>>>> 55fc1ec621323e28640dad3f3bc5ff6fd14cfa03
         </View>
       </SafeAreaView>
     );
@@ -76,7 +58,6 @@ export default class Match extends Component {
 
 
 const styles = StyleSheet.create({
-<<<<<<< HEAD
   logoContainer: {
     alignItems: 'center',
     justifyContent: 'center',
@@ -101,33 +82,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     flex: 3,
   },
-=======
-    logoContainer: {
-        alignItems: "center",
-        justifyContent: "center",
-        flex: 0.3,
-        marginTop: "20%",
-        marginBottom: "10%"
-      },
-      logo: {
-        width: 256,
-        height: 70,},
-        container: {
-            flex: 1,
-            backgroundColor: "white",
-            flexDirection: "column",
-            alignSelf: "center",
-            justifyContent: "space-between",
-            width: "100%",
-            height: "100%"
-          },
-          imageContainer: {
-            marginTop: "10%",
-            alignItems: "center",
-            justifyContent: "center",
-            flex: 4},
-
->>>>>>> 55fc1ec621323e28640dad3f3bc5ff6fd14cfa03
   image: {
     width: 300,
     height: 300
@@ -137,10 +91,7 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontWeight: "bold",
     textAlign: "center",
-<<<<<<< HEAD
     marginTop: 50,
-=======
->>>>>>> 55fc1ec621323e28640dad3f3bc5ff6fd14cfa03
     opacity: 0.9
   },
   description: {
@@ -153,17 +104,7 @@ const styles = StyleSheet.create({
     flex: 2,
     width: "75%",
     justifyContent: "center",
-<<<<<<< HEAD
     alignSelf: "center",
-=======
-    alignSelf: "center"
-  },
-  name: {
-    color: "black",
-    fontSize: 16,
-    textAlign: "center",
-    fontWeight: "bold"
->>>>>>> 55fc1ec621323e28640dad3f3bc5ff6fd14cfa03
   },
   descriptionContainerOne: {
     marginTop: 10,
